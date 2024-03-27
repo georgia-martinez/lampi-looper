9079751baaa1c39f9db3779bbc8cae466309c941
Georgia Martinez <gcm49@case.edu>
2024-03-27 21:52:51 +0000

    Buttons update message to be published when clicked

--- Lampi/lampi/lampi_app.py
+++ Lampi/lampi/lampi_app.py
@@ -12,7 +12,7 @@ from kivy.uix.button import Button
 from kivy.uix.slider import Slider
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
-from kivy.properties import NumericProperty, AliasProperty, BooleanProperty
+from kivy.properties import BooleanProperty
 from kivy.clock import Clock
 from paho.mqtt.client import Client
 from lamp_common import *
@@ -23,9 +23,15 @@ class Color(Enum):
     GREEN = (0, 1, 0, 1)
     BLUE = (0, 0, 1, 1)
 
+    def id(self):
+        return list(Color).index(self)
+
 class BeatButton(Button):
-    def __init__(self, **kwargs):
+
+    def __init__(self, id, **kwargs):
         super(BeatButton, self).__init__(**kwargs)
+
+        self.id = id
 
         self.colors = [
             Color.GRAY,
@@ -36,29 +42,35 @@ class BeatButton(Button):
 
         self.color_id = 0
         self.curr_color = Color.GRAY
-        self._toggle_color()
+        self._set_color()
 
-    def on_press(self):
+    def toggle_color(self):
         if self.color_id != len(self.colors) - 1:
             self.color_id += 1
         else:
             self.color_id = 0
 
-        self._toggle_color()
+        self._set_color()
 
-    def _toggle_color(self):
+    def _set_color(self):
         new_color = self.colors[self.color_id]
 
         self.background_color = new_color.value
         self.curr_color = new_color
 
-class MainLayout(BoxLayout):
-    def __init__(self, **kwargs):
-        super(MainLayout, self).__init__(**kwargs)
-        
-        self.orientation = 'vertical'
+class LampiApp(App):
 
-        # Adding the bpm slider
+    NETWORK_PIN = 17
+    network_butttoggle_colored = BooleanProperty(False)
+    network_popup_open = BooleanProperty(False)
+
+    groove = [0 for _ in range(16)]
+
+    def build(self):
+        
+        layout = BoxLayout(orientation="vertical")
+
+        # Adding the bpm slider and label
         self.bpm_slider = Slider(min=0, max=200, value=100, step=1)
         self.bpm_slider.size_hint_y = None
         self.bpm_slider.height = 50
@@ -70,36 +82,40 @@ class MainLayout(BoxLayout):
         self.bpm_label.height = 20
         self.bpm_slider.bind(value=self.update_bpm_label)
 
-        self.add_widget(self.bpm_slider)
-        self.add_widget(self.bpm_label)
+        layout.add_widget(self.bpm_slider)
+        layout.add_widget(self.bpm_label)
 
         # Adding the button grid
         self.button_grid = GridLayout(cols=4, spacing=5)
 
         for i in range(16):
-            btn = BeatButton()
+            btn = BeatButton(i)
+            btn.bind(on_press=self.on_beat_button_press)  # Bind update_thing method to toggle_color event
             self.button_grid.add_widget(btn)
 
-        self.add_widget(self.button_grid)
+        layout.add_widget(self.button_grid)
 
-    def update_bpm_label(self, instance, value):
-        self.bpm_label.text = f"BPM: {int(value)}"
-
-class LampiApp(App):
-
-    network_button_pressed = BooleanProperty(False)
-    network_popup_open = BooleanProperty(False)
-
-    def build(self):
-        return MainLayout()
+        return layout
 
     def on_start(self):
         self.set_up_network_popup()
 
+    def update_bpm_label(self, instance, value):
+        self.bpm_label.text = f"BPM: {int(value)}"
+
+    def on_beat_button_press(self, instance):
+        instance.toggle_color()
+        self.groove[instance.id] = instance.curr_color.id()
+        self.publish_state_change()
+
+    def publish_state_change(self):
+        msg = {"client": "client", "groove": self.groove, "bpm": self.bpm_label.text}
+        print(msg)
+
     def set_up_network_popup(self):
         self.pi = pigpio.pi()
-        self.pi.set_mode(17, pigpio.INPUT)
-        self.pi.set_pull_up_down(17, pigpio.PUD_UP)
+        self.pi.set_mode(self.NETWORK_PIN, pigpio.INPUT)
+        self.pi.set_pull_up_down(self.NETWORK_PIN, pigpio.PUD_UP)
         Clock.schedule_interval(self._poll_GPIO, 0.05)
         self.network_popup = self._build_network_popup()
         self.network_popup.bind(on_open=self.update_popup_ip_address)
@@ -117,7 +133,7 @@ class LampiApp(App):
         msg = "{}: {}\nDeviceID: {}".format(interface, ipaddr, deviceid)
         instance.content.text = msg
 
-    def on_network_button_pressed(self, instance, value):
+    def on_network_butttoggle_colored(self, instance, value):
         if value:
             if not self.network_popup_open:
                 self.network_popup.open()
@@ -127,4 +143,4 @@ class LampiApp(App):
                 self.network_popup_open = False
 
     def _poll_GPIO(self, dt):
-        self.network_button_pressed = not self.pi.read(17) # gpio17 is the rightmost button
+        self.network_butttoggle_colored = not self.pi.read(self.NETWORK_PIN) # gpio17 is the rightmost button
